08af736ae5ef9ab6c7d9afcc365283de7225f914
Marvin6262 <marvinaj12@gmail.com>
2024-10-10 14:39:47 +0700

    update feature

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -273,7 +273,7 @@ export const createPaymentVA = async (req: Request, res: Response) => {
       const requestBody = update_payment_transaction?.dataValues;
 
       const response = await axios.post(
-        `http://localhost:9000/v1/productPurchase/receivePayment`,
+        `https://apipaymentservice.skyparking.online/v1/productPurchase/receivePayment`,
         requestBody
       );
 
